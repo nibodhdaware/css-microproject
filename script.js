696db405f77b09ea29aef6316686d0588ae0b884
Nibodh Daware <nibod1248@gmail.com>
2022-11-14 12:59:48 +0530

    added about page
    Co-authored-by: yashshahir0111 <yashshahir0111@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-function showMore() {
-    document.getElementById("sect").style.display = "block";
-    document.getElementById("view-more").style.display = "none";
-    document.getElementById("view-less").style.display = "block";
-}
-function showLess() {
-    document.getElementById("sect").style.display = "none";
-    document.getElementById("view-more").style.display = "block";
-    document.getElementById("view-less").style.display = "none";
-}
+// function showMore() {
+//     document.getElementById("sect").style.display = "block";
+//     document.getElementById("view-more").style.display = "none";
+//     document.getElementById("view-less").style.display = "block";
+// }
+// function showLess() {
+//     document.getElementById("sect").style.display = "none";
+//     document.getElementById("view-more").style.display = "block";
+//     document.getElementById("view-less").style.display = "none";
+// }
 
 function showCert() {
-    document.getElementById("containerCat").style.display = "grid";
-    document.getElementById("show-certificates").style.display = "none";
-    document.getElementById("hide-certificates").style.display = "block";
+    window.location.href = "certificates.html";
 }
-
+/*
 function hideCert() {
     document.getElementById("containerCat").style.display = "none";
-    document.getElementById("show-certificates").style.display = "block";
-    document.getElementById("hide-certificates").style.display = "none";
+}*/
+
+function showAcadem() {
+    window.location.href = "qualifications.html";
+}
+
+function showPapers() {
+    window.location.href = "papers.html";
+}
+
+function showCurricular() {
+    window.location.href = "curricular.html";
+}
+
+function showAbout() {
+    window.location.href = "about.html";
+}
+
+function showContact() {
+    window.location.href = "contact.html";
 }
